4efd35381943d42ddb6755fc4dc3ca891c2fb204
AChiofalo <achiofalo1998@gmail.com>
2024-01-25 18:38:39 +0100

    Aggiunto scheletro autenticazione vendor e customer

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 "use strict";
 
 
-const Normalizer = require('./normalize/normalizer.js');
-
 const logger = require('morgan');   //Logger
 const express = require('express'); //Server
 const path = require('path');
@@ -11,21 +9,34 @@ const path = require('path');
 const passport = require('passport'); // auth middleware
 const LocalStrategy = require('passport-local').Strategy; // username and password for login
 const session = require('express-session');
-const FileStore = require('session-file-store')(session); 
+//const FileStore = require('session-file-store')(session); 
 
 const app = express();
-const port = 3000; 
-app.listen(port, () => {`Listening on localhost:${port}`});
+
 
 const productsRouter = require('./routes-API/products');
 const vendorsRouter = require('./routes-API/vendors');
 const sessionsRouter = require('./routes-API/sessions');
 
-//QUI VANNO MIDDLEWARE
+//QUI VANNO MIDDLEWARE GENERICHE
 app.use(logger('short'));
-app.use(express.static('public'));
+
 app.use(express.json());
 
+app.use(session({
+  //store: new FileStore(), // by default, Passport uses a MemoryStore to keep track of the sessions - if you want to use this, launch nodemon with the option: --ignore sessions/
+  secret: 'a secret sentence not to share with anybody and anywhere, used to sign the session ID cookie',
+  resave: false,
+  saveUninitialized: true,
+  // removing the following line will cause a browser's warning, since session cookie
+  // cross-site default policy is currently not recommended
+  cookie: { 
+    sameSite: 'lax',
+    magAge: 1000 * 60
+  }
+}));
+
+app.use(express.static('public'));
 //ROUTES API
 app.use('/api/sessions', sessionsRouter);
 app.use('/api/vendors', vendorsRouter);
@@ -33,27 +44,43 @@ app.use('/api/products', productsRouter);
 //-------
 //ROUTES BASE
 app.get('*', (req,res)=> {     
+    console.log(req.session);
     res.sendFile(path.resolve(__dirname,'public', 'index.html'));
 });
 
 
+const isVendor = (req, res, next) => {
+  if(req.isAuthenticated() && req.user.type == 'vendor'){
+      return next();
+  }
+  return res.status(401).json({"statusCode" : 401, "message" : "not authenticated"});
+}
 
-//const FileStore = require('session-file-store')(session);
+const isCustomer = (req, res, next) => {
+  if(req.isAuthenticated() && req.user.type == 'customer'){
+      return next();
+  }
+  return res.status(401).json({"statusCode" : 401, "message" : "not authenticated"});
+}
 
-// set up the "username and password" login strategy
-// by setting a function to verify username and password
-passport.use('vendor',new LocalStrategy(
-  async function(username, password, done) {
-    const res = await vendorDao.getVendor(username, password) 
-      if (!res.user) {
-        return done(null, false, { message: 'Incorrect username.' });
-      }
-      if (!res.check) {
-        return done(null, false, { message: 'Incorrect password.' });
-      }
-      return done(null, user);
-    })
-);
+
+
+const verify = async (username, password, done) => {
+  try{
+    const res = await vendorDao.getVendor(username, password);
+    return done(null,res.vendor); //Username e password corretti
+
+  }catch(err){
+    if(err.code === 404) return done(null,false); //Username errato
+
+    if(err.code === 401) return done(null,false); //Password errata
+
+    return done(err); //Errore generico
+  }
+}
+
+passport.use('vendor', new LocalStrategy(verify)); //Primo parametro sbagliato?
+ 
 
 passport.use('customer',new LocalStrategy(
   async function(username, password, done) {
@@ -79,27 +106,14 @@ passport.deserializeUser(function(id, done) {
   });
 });
 
-
-passport.serializeUser((user, done) => {
-  done(null, user.username); 
-});
-
-passport.deserializeUser((serializedUser, done) => {
-  if(serializedUser.type === "customer")
-});
+*/
 
 
-app.use(session({
-  store: new FileStore(), // by default, Passport uses a MemoryStore to keep track of the sessions - if you want to use this, launch nodemon with the option: --ignore sessions/
-  secret: 'a secret sentence not to share with anybody and anywhere, used to sign the session ID cookie',
-  resave: false,
-  saveUninitialized: false,
-  // removing the following line will cause a browser's warning, since session cookie
-  // cross-site default policy is currently not recommended
-  cookie: { sameSite: 'lax' }
-  
-}));
+
 
 app.use(passport.initialize());
 app.use(passport.session());
  
+
+const port = 3000; 
+app.listen(port, () => {`Listening on localhost:${port}`});
